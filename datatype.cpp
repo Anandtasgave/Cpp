4a65e313b3195260c45f1fda9e0a26ff9b902596
Anandtasgave <149595717+Anandtasgave@users.noreply.github.com>
2024-03-01 18:53:32 -0800

    Add files via upload

---
+++ datatype.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=79;
+    float f;
+    char c;
+    bool b;
+    cout<<"size of int is "<<sizeof(a)<<endl;
+    cout<<"size of float is "<<sizeof(f)<<endl;
+    cout<<"size of char is "<<sizeof(c)<<endl;
+    cout<<"size of bool is "<<sizeof(b)<<endl;
+}
