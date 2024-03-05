688b45ee14f3c817ac1dac896a2666316bdc83c1
Anandtasgave <149595717+Anandtasgave@users.noreply.github.com>
2024-03-05 21:52:52 +0530

    Add files via upload

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << n << " is even no.";
+    }
+    else
+    {
+        cout << n << " is odd no.";
+    }
+}
