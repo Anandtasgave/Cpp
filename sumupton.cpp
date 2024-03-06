3d43fa385c242b39819bf6361a4031d617f30a7a
Anandtasgave <149595717+Anandtasgave@users.noreply.github.com>
2024-03-06 23:09:22 +0530

    Add files via upload

---
+++ sumupton.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i,sum=0;
+    cin>>n;
+
+    for(i=0;i<=n;i++)
+    {
+        sum+=i;
+    }
+
+    cout<<"Sum upto "<<n<<" is "<<sum;
+}
